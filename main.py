e758e0dcc74ab2cffb1c76a08b6dce2429fca50e
LiadUziel <84779407+LiadUziel@users.noreply.github.com>
2021-09-07 15:48:40 +0300

    Small cosmetic change

--- main.py
+++ main.py
@@ -118,10 +118,10 @@ def show_some(player, dealer):
 
 
 def show_all(player, dealer):
-    print("\nDealer's Hand: ", *dealer.cards, sep='\n ')
+    print("\nDealer's Hand: ", *dealer.cards, sep='\n')
     print(" Dealer's Hand =", dealer.value)
 
-    print("\nPlayer's Hand: ", *player.cards, sep='\n ')
+    print("\nPlayer's Hand: ", *player.cards, sep='\n')
     print(" Player's Hand =", player.value)
     print()
 
